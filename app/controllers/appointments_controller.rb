7d1dea52a03cbb5229d75b70c734fad9c2ef7beb
Christina Nguyen <christina.nguyen.cn@gmail.com>
2016-05-31 21:27:54 -0400

    add Puma and postgresql for ease for deploying to heroku and also added appointments controller and model

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,34 @@
+class AppointmentsController < ApplicationController
+   
+  def index
+    appointments = Appointment.all
+
+    if start_time = params[:start_time]
+      appointments = appointments.where(start_time: start_time)
+    end
+
+    if end_time = params[:end_time]
+      appointments = appointments.where(end_time: end_time)
+    end
+    render json: appointments, status: 200
+  end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+
+    if appointment.save
+      render json: appointment, status: 201, location: appointment
+    else
+      render json: appointment.errors, status: 422
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def appointment_params
+      params.require(:appointment).permit(:first_name, :last_name, :start_time, :end_time, :comments)
+    end
+end
