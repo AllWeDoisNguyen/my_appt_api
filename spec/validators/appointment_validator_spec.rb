a3e0624af90d7c5bb3abb889a850e7aef42c867f
Christina Nguyen <christina.nguyen.cn@gmail.com>
2016-06-03 18:32:10 -0400

    This is where I make the validator only search for the relevant appointments in the nearest decade. This is helpful because then the validator is more efficiently iterating through the database rather than going through the entire database every query. line 7 in appointment validator.

--- spec/validators/appointment_validator_spec.rb
+++ spec/validators/appointment_validator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "AppointmentValidator" do
   before do
-    @existing_appointment = Appointment.new(start_time: "11/30/17 7:30", end_time: "11/30/17 7:35", first_name: "Stanley", last_name: "Man")
+    @existing_appointment = Appointment.new(start_time: "11/30/17 7:30", end_time: "11/30/17 7:35", first_name: "Stanley", last_name: "Man", day: "2017-11-30T07:03:00+00:00")
   end
 
   it "should validate the start and end time existence" do
@@ -12,6 +12,7 @@ describe "AppointmentValidator" do
   end
 
   it "should check if appointment time is in the future" do
+    @existing_appointment.save
     @appointment = Appointment.new(start_time: "11/30/12 7:30", end_time: "11/30/12 7:35", first_name: "ba", last_name: "an")
     AppointmentValidator.new.validate(@appointment)
     expect(@appointment.errors[:time]).to include("Appointment must be made in future")
